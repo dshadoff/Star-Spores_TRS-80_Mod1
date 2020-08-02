b908fbc2def6f9f30d2223ca810ed7430a0c79bd
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-08-01 22:16:12 -0400

    Distributor message
    - added message for 'distributed by C-Soft'

---
+++ src/distrb.asm
@@ -0,0 +1,10 @@
+	ORG	4F00H
+ENTRY	EQU	5460H
+
+DISTRB	DEFM	'Press <SPACE> for instructions.  To play, press "1" or "2" '
+	DEFM	'for the number of players.  Copyright (C) 1982, by '
+	DEFM	'Dave Shadoff.  All rights reserved.  Distributed by: C-Soft, '
+	DEFM	'Mississauga, Ontario, Canada.          '
+	DEFB	00
+
+	END	ENTRY
--- src/make.bat
+++ src/make.bat
@@ -1,3 +1,4 @@
+zmac --oo lst,cas,cmd,bds -c distrb.asm
 zmac --oo lst,cas,cmd,bds -c entry.asm
 zmac --oo lst,cas,cmd,bds -c titlep.asm
 zmac --oo lst,cas,cmd,bds -c spores.asm
