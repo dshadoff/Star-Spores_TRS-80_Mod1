8dad1952ad5864976c00c492149e8dede643da6a
David Shadoff <46657586+dshadoff@users.noreply.github.com>
2020-08-02 02:24:56 -0400

    Effects code
    Interstitial special effects, explosion, high score etc.

--- src/make.bat
+++ src/make.bat
@@ -1,4 +1,5 @@
 zmac --oo lst,cas,cmd,bds -c distrb.asm
 zmac --oo lst,cas,cmd,bds -c entry.asm
-zmac --oo lst,cas,cmd,bds -c titlep.asm
 zmac --oo lst,cas,cmd,bds -c spores.asm
+zmac --oo lst,cas,cmd,bds -c titlep.asm
+zmac --oo lst,cas,cmd,bds -c efects.asm
